d223885721632eb8a164dd77e6bdcfc2f86808e0
Jason Henson <jason.a.henson@gmail.com>
2022-01-26 08:54:47 -0500

    add files to dev branch

---
+++ MusicDatabaseRedux.Models/PlaylistModels/PlaylistCreate.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MusicDatabaseRedux.Models.PlaylistModels
+{
+    internal class PlaylistCreate
+    {
+    }
+}
