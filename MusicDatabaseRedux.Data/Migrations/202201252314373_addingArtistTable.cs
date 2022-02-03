22bb1752027d1226a833e9f2d968694a9cef9d92
Brandon G <goewertb@yahoo.com>
2022-02-03 12:53:01 -0500

    Revert "adding"
    This reverts commit 9f5ec81f1ff6d9060cf42dcd296359a01b768680.

--- MusicDatabaseRedux.Data/Migrations/202201252314373_addingArtistTable.cs
+++ MusicDatabaseRedux.Data/Migrations/202201252314373_addingArtistTable.cs
@@ -1,7 +1,8 @@
 ﻿namespace MusicDatabaseRedux.Data.Migrations
 {
+    using System;
     using System.Data.Entity.Migrations;
-
+    
     public partial class addingArtistTable : DbMigration
     {
         public override void Up()
@@ -9,16 +10,17 @@
             CreateTable(
                 "dbo.Artist",
                 c => new
-                {
-                    ArtistId = c.Int(nullable: false, identity: true),
-                    OwnerId = c.Guid(nullable: false),
-                    Name = c.String(nullable: false),
-                    NumberOfMembers = c.Int(nullable: false),
-                    IsAlive = c.Boolean(nullable: false),
-                })
+                    {
+                        ArtistId = c.Int(nullable: false, identity: true),
+                        OwnerId = c.Guid(nullable: false),
+                        Name = c.String(nullable: false),
+                        NumberOfMembers = c.Int(nullable: false),
+                        IsAlive = c.Boolean(nullable: false),
+                    })
                 .PrimaryKey(t => t.ArtistId);
+            
         }
-
+        
         public override void Down()
         {
             DropTable("dbo.Artist");
--- MusicDatabaseRedux.Data/Migrations/Configuration.cs
+++ MusicDatabaseRedux.Data/Migrations/Configuration.cs
@@ -1,6 +1,9 @@
 ﻿namespace MusicDatabaseRedux.Data.Migrations
 {
+    using System;
+    using System.Data.Entity;
     using System.Data.Entity.Migrations;
+    using System.Linq;
 
     internal sealed class Configuration : DbMigrationsConfiguration<MusicDatabaseRedux.Data.ApplicationDbContext>
     {
