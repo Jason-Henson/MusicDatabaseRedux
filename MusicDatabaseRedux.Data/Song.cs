5ec5e7ea18d83b17d380781bbc0ac96eb99b051c
Doug Revell <doug.revelljr@gmail.com>
2022-02-03 08:53:11 -0500

    removeplaylistclass

--- MusicDatabaseRedux.Data/Song.cs
+++ MusicDatabaseRedux.Data/Song.cs
@@ -11,11 +11,13 @@ namespace MusicDatabaseRedux.Data
     public class Song
     {
         [Key]
-        public int Id { get; set; }
+        public int SongId { get; set; }
         public Guid OwnerId { get; set; }
         public string SongName { get; set; }
+
         [ForeignKey(nameof(Artist))]
         public int ArtistId { get; set; }
+        public virtual Artist Artist { get; set; }
         public string Genre { get; set; }
     }
 }
